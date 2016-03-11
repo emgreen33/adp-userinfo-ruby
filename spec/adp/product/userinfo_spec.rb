79411f75a961321f2dd03846530f394985988654
Napier-Contractor, Junior (CORP) <Junior.Napier-Contractor@adp.com>
2016-03-11 12:52:41 -0500

    update helper test rspec

--- spec/adp/product/userinfo_spec.rb
+++ spec/adp/product/userinfo_spec.rb
@@ -5,7 +5,11 @@ describe Adp::Product::Userinfo do
     expect(Adp::Product::Userinfo::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  clientcredential_config = Adp::Connection::ClientCredentialConfiguration.new({});
+  connection = Adp::Connection::ApiConnectionFactory::createConnection(clientcredential_config)
+
+  it 'create new Helper item' do
+    # expect(true).to eq(true)
+    expect(Adp::Product::Userinfo::Helper.new(connection)).not_to be nil
   end
 end
